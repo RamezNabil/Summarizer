46529e24fc5991b6b330ef4edb0b7028443dc71c
Abanoub Moris <37875569+AbanoubMoris@users.noreply.github.com>
2021-04-09 19:16:37 +0200

    gui

--- ReadDocument.py
+++ ReadDocument.py
@@ -1,6 +1,7 @@
 import PyPDF2
 
 def ReadingFromPDF(filename,f,l):
+
     pdfFile = open(filename, "rb")
     pdfReader = PyPDF2.PdfFileReader(pdfFile)
     numOfPages = pdfReader.numPages
@@ -8,11 +9,9 @@ def ReadingFromPDF(filename,f,l):
     for i in range(f-1,l):
         page = pdfReader.getPage(i)
         pdf += page.extractText()
-
-    return pdf
+        
     pdfFile.close()
-
-    pdf = ReadingFromPDF("Assignment.pdf",1,7)
+    
     preprocess_text = pdf.strip().replace("\n","")
     t5_prepared_Text = "summarize: "+preprocess_text
     pdf = t5_prepared_Text
